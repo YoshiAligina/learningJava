87d747943104f2588d5413d452ecfddeaa6994c9
Yoshita Aligina <113857935+YoshiAligina@users.noreply.github.com>
2024-05-11 09:58:53 -0400

    update files struct

---
+++ learnspringframeworkBasic/src/main/java/com/project/learnspringframeworkBasic/LearnspringframeworkBasicApplication.java
@@ -0,0 +1,13 @@
+package com.project.learnspringframeworkBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringframeworkBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringframeworkBasicApplication.class, args);
+	}
+
+}
---
+++ learnspringframeworkBasic/src/test/java/com/project/learnspringframeworkBasic/LearnspringframeworkBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.learnspringframeworkBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringframeworkBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
